e829bdd247ce01003aee1e43f4d3691e87bf9b0d
JpLyg <japjolla@gmail.com>
2025-09-25 17:40:00 -0700

    return 1st word

--- main.go
+++ main.go
@@ -1,12 +1,37 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+
+		if !scanner.Scan() {
+			break
+		}
+		line := scanner.Text()
+		line = strings.ToLower(strings.TrimSpace(line))
+		parts := strings.Fields(line)
+		if len(parts) == 0 {
+			continue
+		}
+		fmt.Printf("Your command was: %s\n", parts[0])
+		if err := scanner.Err(); err != nil {
+			fmt.Println("Error:", err)
+		}
+		if parts[0] == "exit" {
+			break
+		}
+	}
+
 }
 
 func cleanInput(text string) []string {
